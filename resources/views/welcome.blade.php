7c5e70bd890bee133d13c548539be073dacae42f
billah1 <billah9578@gmail.com>
2024-04-21 17:20:56 +0600

    laravel react file setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    @viteReactRefresh
+    @vite(['resources/js/index.css', 'resources/js/Example.jsx'])
+</head>
+<body>
+<div id="example"></div>
+</body>
+</html>
